33a3c14c12597e7349daf8baaa401f842553d381
Tilman Adler <tilman-git@t-animal.de>
2019-01-26 14:43:06 +0100

    Add a component to input new messages

--- DoChat/src/app/app.module.ts
+++ DoChat/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
 import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InputComponent,
     MessagesComponent
   ],
   imports: [
---
+++ DoChat/src/app/input/input.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { WebsocketServiceService } from '../websocket-service.service';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+
+  @ViewChild('input')
+  textInput: ElementRef<HTMLTextAreaElement>
+
+  constructor(
+    private socket: WebsocketServiceService
+  ) { }
+
+  inputChanged(){
+    this.socket.sendMessage(this.textInput.nativeElement.value);
+    this.textInput.nativeElement.value = "";
+  }
+}
