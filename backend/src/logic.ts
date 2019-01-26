5f6f1d6d7525853cbac2a1afc11d0663a34353fc
Tilman Adler <tilman-git@t-animal.de>
2019-01-26 14:35:50 +0100

    Improve backend handling of sockets

--- backend/src/logic.ts
+++ backend/src/logic.ts
@@ -1,14 +1,25 @@
 import * as WebSocket from 'ws';
 
-const sockets: WebSocket[] = [];
+let sockets: WebSocket[] = [];
 
 export function initSocket(newSocket: WebSocket) {
     sockets.push(newSocket);
 
     (<any>newSocket).on('message', messageHandler);
-    // newSocket.on('close')
+
+    newSocket.on('close', () => {
+        sockets = sockets.filter(elem => elem !== newSocket);
+    });
+
+    newSocket.on('error', () => {
+        sockets = sockets.filter(elem => elem !== newSocket);
+    });
 }
 
 function messageHandler(this: WebSocket, data: WebSocket.Data){
-    sockets.forEach(socket => socket.send(data));
+    sockets.forEach(socket => {
+        if(socket.readyState === this.OPEN){
+            socket.send(data);
+        }
+    });
 }
