d38b2dd59daa9b849e47c9dabb383564d422084c
Tilman Adler <tilman-git@t-animal.de>
2019-02-17 13:06:16 +0100

    Implement custom playback controls

--- DoChat/src/app/audio-message/audio-message.component.ts
+++ DoChat/src/app/audio-message/audio-message.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+
+type State = 'stopped' | 'playing' | 'paused';
 
 @Component({
   selector: 'app-audio-message',
@@ -7,7 +9,40 @@ import { Component, Input } from '@angular/core';
 })
 export class AudioMessageComponent {
 
+  duration: number = 0;
+  currentTime: number = 0;
+  percentage: number = 0;
+  state: State = 'stopped';
+
   @Input()
   message: TextMessage
 
+  @ViewChild('player')
+  player: ElementRef<HTMLAudioElement>;
+
+  updateUI() {
+    this.duration = this.player.nativeElement.duration;
+    this.currentTime = this.player.nativeElement.currentTime;
+    this.percentage = this.currentTime / this.duration * 100;
+
+    if(this.duration - this.currentTime < 0.01){
+      this.state = 'stopped';
+    }
+  }
+
+  play(){
+    this.state = 'playing'
+    this.player.nativeElement.play();
+  }
+
+  stop(){
+    this.state = 'stopped';
+    this.player.nativeElement.pause();
+    this.player.nativeElement.currentTime = 0;
+  }
+
+  pause(){
+    this.state = 'paused';
+    this.player.nativeElement.pause();
+  }
 }
