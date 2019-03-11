71abfa651326d2cb84d3e8f0910a65054c1f847d
Tilman Adler <tilman-git@t-animal.de>
2019-03-11 19:55:16 +0100

    Persist receiver name to local storage

--- DoChat/src/app/config-holder.service.ts
+++ DoChat/src/app/config-holder.service.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable, Inject } from '@angular/core';
+import { Subject, concat, of } from 'rxjs';
+import { StorageService, LOCAL_STORAGE } from 'angular-webstorage-service';
+
+const RECEIVER_NAME_KEY = 'receiver-name';
+const SERVER_KEY = 'server-address';
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +13,39 @@ export class ConfigHolderService {
   private name = new Subject<string>();
   private server = new Subject<string>();
 
+  constructor(
+    @Inject(LOCAL_STORAGE) private storage: StorageService
+ ){ }
+
+
   setName(name: string) {
     if(name.trim() === ''){
       name = 'Vorname Nachname';
     }
+    this.storage.set(RECEIVER_NAME_KEY, name);
     this.name.next(name);
   }
 
   setServer(server: string) {
+    this.storage.set(SERVER_KEY, server);
     this.server.next(server);
   }
 
   getName(){
-    return this.name.asObservable();
+    const storedName: string = this.storage.get(RECEIVER_NAME_KEY);
+
+    if(storedName === null)
+      return this.name.asObservable();
+
+    return concat(of(storedName), this.name.asObservable());
   }
 
   getServer() {
-    return this.name.asObservable();
+    const storedServer: string = this.storage.get(SERVER_KEY);
+
+    if(storedServer === null)
+      return this.server.asObservable();
+  
+    return concat(of(storedServer), this.server.asObservable());
   }
 }
